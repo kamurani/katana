94bd6c941ccb87480c7e9209dfce3b7c45ea143f
kamurani <cameronmcmenamie@gmail.com>
2025-02-27 14:51:40 +1100

    add methods to PBSServer class

---
+++ scripts/pbs_server.py
@@ -0,0 +1,191 @@
+import subprocess
+import os
+
+
+
+from functools import partial
+from typing import Tuple
+from pathlib import Path
+
+from sshconf import read_ssh_config
+from os.path import expanduser
+
+
+
+class PBSServer:
+    """Class to interact with the PBS server.
+
+    Parameters
+    ----------
+    remotehost : str
+        The hostname of the remote server.
+    print_output : bool
+        Whether to print the output of the commands.
+
+    """
+    def __init__(
+        self, 
+        remotehost: str = "katana",
+        print_output: bool = False,
+        verbose: bool = True,
+
+    ): 
+        self.remotehost = remotehost
+        self.print_output = print_output
+        self.verbose = verbose
+
+
+
+        #initialise the print_stdout decorator with print_output set to True
+
+        # TODO: 
+        # figure out how to add a decorator which, depending on a class attribute (self.print_output), 
+        # will do different things.
+        # self.print_stdout = partial(print_stdout, print_output=True)
+
+        
+        # Find remotehost in the known_hosts file
+        SSH_CONFIG_PATH = Path.home().resolve() / ".ssh" / "config"
+        assert SSH_CONFIG_PATH.is_file(), f"SSH config file not found at {SSH_CONFIG_PATH}"
+
+        # Get hostnames from ssh config
+        with open(SSH_CONFIG_PATH, "r") as f:
+            ssh_config = f.read()
+
+        hostnames = []
+        for line in ssh_config.split("\n"):
+            if line.startswith("Host "):
+                current = line.split()[1]
+                hostnames.append(current)
+
+            
+        if self.verbose: print(f"Found {len(hostnames)} hostnames in ssh config")
+
+        # Scan for the remotehost in the known_hosts file
+
+
+
+    """Decorator for stdout and stderr collection."""
+    def print_stdout(func):
+        def decorated(self, *args, **kwargs):
+            stdout, stderr = None, None
+            try:
+                stdout, stderr = func(self, *args, **kwargs)
+            except Exception as e:
+                print(e)
+            
+            if self.print_output: 
+                print(stdout)
+                print(stderr)
+            return stdout, stderr
+        return decorated
+    
+    @print_stdout
+    def ssh_execute(self, cmd):
+        cmd = ["ssh", self.remotehost, cmd]
+        captured = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=False)
+        stdout = captured.stdout.read().decode()
+        stderr = captured.stderr.read().decode()
+        return stdout, stderr
+    
+    def ssh_jump_execute(self, cmd: str, target_node: str, login_node: str = None):
+        login_node = self.remotehost if login_node is None else login_node
+        cmd = ["ssh", "-J", login_node, f"{target_node}", cmd]
+        captured = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=False)
+        stdout = captured.stdout.read().decode()
+        stderr = captured.stderr.read().decode()
+        return stdout, stderr
+    
+    def check_gpu(
+        self, 
+        node: str = None, 
+        job_id: str = None, 
+    ) -> Tuple[str, str]:
+        """Check the GPU usage on a node."""
+        cmd = "nvidia-smi"
+        if node is None: 
+            if job_id is None:
+                raise ValueError("Either node or job_id must be provided.")
+            info_dict = self.job_info(job_id)
+            node = info_dict["node"]
+        stdout, stderr = self.ssh_jump_execute(cmd, target_node=node)
+        return stdout, stderr
+    
+    @print_stdout
+    def qstat(
+        self,
+        job_id: str = None,
+        arguments: list = ["-n", "-f"],
+
+    ):
+        """Get information about jobs in the queue.
+        
+        Job Status codes: 
+        H - Held
+        Q - Queued
+        R - Running
+        C - Completed
+        E - Exiting
+
+
+        """
+        cmd = "qstat"
+        if job_id is not None:
+            cmd += f" {job_id}"
+
+        cmd = " ".join([cmd] + arguments)
+        stdout, stderr = self.ssh_execute(cmd)
+        return stdout, stderr
+    
+    @print_stdout
+    def pstat(self):
+        """Get overview of the compute nodes and list of jobs running on each node."""
+        cmd = "pstat"
+        stdout, stderr = self.ssh_execute(cmd)
+        return stdout, stderr  
+
+    @print_stdout
+    def pbsnodes(self, node: str):
+        cmd = f"pbsnodes {node}"
+        stdout, stderr = self.ssh_execute(cmd)
+        return stdout, stderr  
+    
+    def job_info(self, job_id: str):
+        """Parse the output of pstat command."""
+        job_id = str(job_id).strip()
+        info_dict = self._parse_pstat(job_id)
+        return info_dict
+
+    def _parse_pstat(
+        self, 
+        job_id: str,
+    ) -> str: 
+        """Parse qstat output for a particular job and return the information."""
+        if job_id is None:
+            raise ValueError("job_id must be provided.")
+        stdout, _ = self.qstat(job_id=job_id)
+        
+        # find job name line 
+        lines = stdout.split("\n")
+        for i, line in enumerate(lines):
+            if line.startswith(job_id):
+                break
+        header = lines[i-2] 
+        info_line = lines[i]
+        node_line = lines[i+1]
+
+        header = header.replace("Job ID", "Job_ID") # to avoid splitting on space
+        header = header.split()
+        fields = info_line.split()
+        assert len(fields) == len(header), f"Parse error: Fields and header mismatch: {len(fields)} vs {len(header)}"
+
+        node_line = node_line.strip()
+        node_name, resources = node_line.split("/")
+
+        status = fields[header.index("S")] 
+        return dict(
+            status=status,
+            node=node_name,
+            resources=resources,
+        )
+
